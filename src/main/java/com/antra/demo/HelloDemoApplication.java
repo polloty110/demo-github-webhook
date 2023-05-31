67d0cdd2bde45f3ea9654b0df634bc88d11b36a9
polloty110 <polloty110@gmail.com>
2023-05-31 14:57:20 -0400

    tomcat

--- src/main/java/com/antra/demo/HelloDemoApplication.java
+++ src/main/java/com/antra/demo/HelloDemoApplication.java
@@ -2,9 +2,10 @@ package com.antra.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloDemoApplication {
+public class HelloDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloDemoApplication.class, args);
